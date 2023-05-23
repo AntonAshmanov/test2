41515a3f28f3df50b8744b5afd2584879f9a65c9
AntonAshmanov <Antosha.v.ashmanov@gmail.com>
2023-05-23 23:25:50 +0300

    creae main.h

---
+++ main.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void func1() {
+    int x = 0;
+
+    std::cout << x << std::endl;
+}
+
+void func2() {
+    int y = 0;
+
+    std::cout << y << std::endl;
+}
+
+int main() {
+
+    func1();
+    func2();
+}
